1ddc49befe32f0c447f68541f7d153d416b89ede
Jason <121495300+jason21715@users.noreply.github.com>
2024-11-03 01:55:02 -0700

    Add new project, Update theme and fonts, Remove contact section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -35,12 +35,11 @@ export class AppComponent {
     this.projects[id].showDesc = !this.projects[id].showDesc;
   }
 
-  projectID: number = 0;
   projects: Project[] = [
     {
       id: 0,
-      title: "LinkStorage",
-      image: "../assets/661shots_so.png",
+      title: "Linkstorage",
+      image: "../assets/1.png",
       videoSrc: "https://www.youtube.com/embed/0C5lZlLnrcs?si=9su4ICICZM77-SvF",
       description:
         "Linkstorage is a web app to store links with screenshots/thumbnails. You can even share your folders to the world. Check one out here: (linkstorage.net/public/1). Linkstorage provides features such as Rich Previews, Folders, Searching, Sharing, Mass editing, Bookmarks and more...",
@@ -62,8 +61,31 @@ export class AppComponent {
 
     {
       id: 1,
+      title: "Project0",
+      image: "../assets/22.png",
+      videoSrc: "",
+      description:
+        "Project0 is an app that help creators work together on a video draft and share files needed for each video edits, this app is private, and made only for friends.",
+      appHref: "",
+      githubHref: "",
+      iconsHTML: [
+        "Angular",
+        "PostgreSQL",
+        "Prisma",
+        "Express",
+        "Node",
+        "Typescript",
+        "HTML",
+        "SCSS",
+      ],
+      showDesc: false,
+      showVid: false,
+    },
+
+    {
+      id: 2,
       title: "minBlog",
-      image: "../assets/296_1x_shots_so.png",
+      image: "../assets/23.png",
       videoSrc: "https://www.youtube.com/embed/J5s9PAta4V0",
       description:
         "minBlog is a reading platform, and a blog sharing platform with short 1500 character blogs. With features like REST API, secure authentication with bcrypt, passport/sessions along with blog searching, filtering, and pagination",
@@ -71,19 +93,6 @@ export class AppComponent {
       appHref: "https://minblog21715.netlify.app/",
       githubHref: "https://github.com/JasonCqq/minBlog",
       iconsHTML: ["React", "MongoDB", "Express", "Node", "Typescript"],
-      showDesc: false,
-      showVid: false,
-    },
-    {
-      id: 2,
-      title: "Jwitter",
-      image: "../assets/111_1x_shots_so.png",
-      videoSrc: "https://www.youtube.com/embed/hJ5OKDj8ZxE",
-      description:
-        "Jwitter is a Twitter clone that replicates the core features of Twitter such as realtime tweets, follow system, and likes. This project was developed from scratch without relying on tutorials to practice full stack skills.",
-      appHref: "https://main--chipper-gnome-4de1e9.netlify.app/",
-      githubHref: "https://github.com/JasonCqq/Jwitter",
-      iconsHTML: ["React", "Typescript", "Firebase"],
       showDesc: false,
       showVid: false,
     },
