e37de74b2523ebe6faa4419b15928c6f16d2abfc
Jason <121495300+jason21715@users.noreply.github.com>
2024-11-10 23:55:42 -0800

    Add new color and design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -31,8 +31,43 @@ export class AppComponent {
     this.projects[id].showVid = !this.projects[id].showVid;
   }
 
-  toggleDescription(id: number): void {
-    this.projects[id].showDesc = !this.projects[id].showDesc;
+  show: boolean = false;
+
+  showMore() {
+    if (this.show === true) {
+      this.projects.push(
+        {
+          id: 3,
+          title: "Jwitter",
+          image: "../assets/jwitter.png",
+          videoSrc: "",
+          description:
+            "Twitter clone that replicates the core features of old Twitter. This project was developed from scratch without relying on tutorials to practice full stack skills.",
+          appHref: "https://main--chipper-gnome-4de1e9.netlify.app/",
+          githubHref: "https://github.com/JasonCqq/Jwitter",
+          iconsHTML: ["React", "Typescript", "Firebase"],
+          showDesc: false,
+          showVid: false,
+        },
+
+        {
+          id: 4,
+          title: "Wimmelbilder",
+          image: "../assets/wimmel.png",
+          videoSrc: "",
+          description:
+            "Very simple where's waldo inspired game, has fun features like leaderboard",
+          appHref: "https://main--nimble-cendol-ef155b.netlify.app/#/",
+          githubHref: "https://github.com/JasonCqq/Wimmelbilder",
+          iconsHTML: ["React", "Typescript", "Firebase"],
+          showDesc: false,
+          showVid: false,
+        }
+      );
+    } else if (this.show === false) {
+      this.projects.pop();
+      this.projects.pop();
+    }
   }
 
   projects: Project[] = [
@@ -42,7 +77,7 @@ export class AppComponent {
       image: "../assets/1.png",
       videoSrc: "https://www.youtube.com/embed/0C5lZlLnrcs?si=9su4ICICZM77-SvF",
       description:
-        "Linkstorage is a web app to store links with screenshots/thumbnails. You can even share your folders to the world. Check one out here: (linkstorage.net/public/1). Linkstorage provides features such as Rich Previews, Folders, Searching, Sharing, Mass editing, Bookmarks and more...",
+        "Store URLs with thumbnails. Has an active userbase and includes many features such as url bank (plain text urls), folder system, folder sharing, link searching/sorting by visits. Visit the live app to see the rest of the features.",
       appHref: "https://linkstorage.net",
       githubHref: "https://github.com/JasonCqq/link-saver",
       iconsHTML: [
@@ -61,11 +96,11 @@ export class AppComponent {
 
     {
       id: 1,
-      title: "Project0",
+      title: "Project0 (Private)",
       image: "../assets/22.png",
       videoSrc: "",
       description:
-        "Project0 is an app that help creators work together on a video draft and share files needed for each video edits, this app is private, and made only for friends.",
+        "Team communication and management platform similar to Slack for video/content creators. Includes real-time collaboration on video drafts, sharing video editing files, activity logs for each member, and commenting",
       appHref: "",
       githubHref: "",
       iconsHTML: [
@@ -88,8 +123,7 @@ export class AppComponent {
       image: "../assets/23.png",
       videoSrc: "https://www.youtube.com/embed/J5s9PAta4V0",
       description:
-        "minBlog is a reading platform, and a blog sharing platform with short 1500 character blogs. With features like REST API, secure authentication with bcrypt, passport/sessions along with blog searching, filtering, and pagination",
-
+        "Blog sharing and reading platform with short blogs (max 1500 chars). With features such as user authentication, and basic CMS features: user profiles, blog searching, commenting, filtering, and pagination",
       appHref: "https://minblog21715.netlify.app/",
       githubHref: "https://github.com/JasonCqq/minBlog",
       iconsHTML: ["React", "MongoDB", "Express", "Node", "Typescript"],
