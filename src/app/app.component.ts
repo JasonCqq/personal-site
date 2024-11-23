9b0027802da4d9fd1070ce90265c09105a4705e1
Jason <121495300+jason21715@users.noreply.github.com>
2024-11-22 19:25:03 -0800

    Add video demos, Fix small styling issues, Update project images, Update website icon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,6 @@ interface Project {
   appHref: string;
   githubHref: string;
   iconsHTML: string[];
-  showDesc: boolean;
   showVid: boolean;
 }
 
@@ -21,6 +20,14 @@ interface Project {
 })
 export class AppComponent {
   constructor(private sanitizer: DomSanitizer) {}
+
+  showE: boolean = false;
+  toggleEmail() {
+    setTimeout(() => {
+      this.showE = false;
+    }, 10000);
+    this.showE = !this.showE;
+  }
 
   // Allow Urls
   safeUrl(url: string): SafeUrl {
@@ -41,13 +48,13 @@ export class AppComponent {
           id: 3,
           title: "Jwitter",
           image: "../assets/jwitter.png",
-          videoSrc: "",
+          videoSrc:
+            "https://www.youtube.com/embed/hJ5OKDj8ZxE?si=3VvpDTXYR4fWYPD_",
           description:
             "Twitter clone that replicates the core features of old Twitter. This project was developed from scratch without relying on tutorials to practice full stack skills.",
           appHref: "https://main--chipper-gnome-4de1e9.netlify.app/",
           githubHref: "https://github.com/JasonCqq/Jwitter",
           iconsHTML: ["React", "Typescript", "Firebase"],
-          showDesc: false,
           showVid: false,
         },
 
@@ -55,13 +62,13 @@ export class AppComponent {
           id: 4,
           title: "Wimmelbilder",
           image: "../assets/wimmel.png",
-          videoSrc: "",
+          videoSrc:
+            "https://www.youtube.com/embed/X6CBm3JiTpw?si=oEIt9r47LMV1-K0H",
           description:
             "Very simple where's waldo inspired game, has fun features like leaderboard",
           appHref: "https://main--nimble-cendol-ef155b.netlify.app/#/",
           githubHref: "https://github.com/JasonCqq/Wimmelbilder",
           iconsHTML: ["React", "Typescript", "Firebase"],
-          showDesc: false,
           showVid: false,
         }
       );
@@ -76,7 +83,7 @@ export class AppComponent {
       id: 0,
       title: "Linkstorage",
       image: "../assets/1.png",
-      videoSrc: "https://www.youtube.com/embed/0C5lZlLnrcs?si=9su4ICICZM77-SvF",
+      videoSrc: "https://www.youtube.com/embed/OrvRE0zM_QY?si=C6OLIz7M43Z7aYKq",
       description:
         "Store URLs with thumbnails. Has an active userbase and includes many features such as url bank (plain text urls), folder system, folder sharing, link searching/sorting by visits. Visit the live app to see the rest of the features.",
       appHref: "https://linkstorage.net",
@@ -91,17 +98,16 @@ export class AppComponent {
         "HTML",
         "SCSS",
       ],
-      showDesc: false,
       showVid: false,
     },
 
     {
       id: 1,
       title: "Project0 (Private)",
-      image: "../assets/22.png",
-      videoSrc: "",
+      image: "../assets/project0.png",
+      videoSrc: "https://www.youtube.com/embed/tRKLOirIqDw?si=cd--hdmPZsVExS-i",
       description:
-        "Team communication and management platform similar to Slack for video/content creators. Includes real-time collaboration on video drafts, sharing video editing files, activity logs for each member, and commenting",
+        "Team communication and management platform similar to Slack for video/content creators. Includes real-time collaboration on video drafts, sharing video editing files, activity logs for each member, and chatrooms for videos",
       appHref: "",
       githubHref: "",
       iconsHTML: [
@@ -114,7 +120,6 @@ export class AppComponent {
         "HTML",
         "SCSS",
       ],
-      showDesc: false,
       showVid: false,
     },
 
@@ -122,13 +127,12 @@ export class AppComponent {
       id: 2,
       title: "minBlog",
       image: "../assets/23.png",
-      videoSrc: "https://www.youtube.com/embed/J5s9PAta4V0",
+      videoSrc: "https://www.youtube.com/embed/J5s9PAta4V0?si=TyCnC-xOPs1JOwRn",
       description:
         "Blog sharing and reading platform with short blogs (max 1500 chars). With features such as user authentication, and basic CMS features: user profiles, blog searching, commenting, filtering, and pagination",
       appHref: "https://minblog21715.netlify.app/",
       githubHref: "https://github.com/JasonCqq/minBlog",
       iconsHTML: ["React", "MongoDB", "Express", "Node", "Typescript"],
-      showDesc: false,
       showVid: false,
     },
   ];
