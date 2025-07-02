7393c1e2f6ed61bc9af76dc8a07c38e1137971ed
jason21715 <121495300+jason21715@users.noreply.github.com>
2025-07-01 22:03:28 -0700

    Remove show more, projects will show by default

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -38,60 +38,6 @@ export class AppComponent {
     this.projects[id].showVid = !this.projects[id].showVid;
   }
 
-  show: boolean = false;
-
-  showMore() {
-    this.show = !this.show;
-    if (this.show === true) {
-      this.projects.push(
-        {
-          id: 3,
-          title: "minBlog",
-          image: "../assets/23.png",
-          videoSrc: "https://www.youtube.com/embed/J5s9PAta4V0?si=TyCnC-xOPs1JOwRn",
-          description:
-            "Blog sharing and reading platform with short blogs (max 1500 chars). With features such as user authentication, and basic CMS features: user profiles, blog searching, commenting, filtering, and pagination",
-          appHref: "https://minblog21715.netlify.app/",
-          githubHref: "https://github.com/JasonCqq/minBlog",
-          iconsHTML: ["React", "MongoDB", "Express", "Node", "Typescript"],
-          showVid: false,
-        },
-
-        {
-          id: 4,
-          title: "Jwitter",
-          image: "../assets/jwitter.png",
-          videoSrc:
-            "https://www.youtube.com/embed/hJ5OKDj8ZxE?si=3VvpDTXYR4fWYPD_",
-          description:
-            "Twitter clone that replicates the core features of old Twitter. This project was developed from scratch without relying on tutorials to practice full stack skills.",
-          appHref: "https://main--chipper-gnome-4de1e9.netlify.app/",
-          githubHref: "https://github.com/JasonCqq/Jwitter",
-          iconsHTML: ["React", "Typescript", "Firebase"],
-          showVid: false,
-        },
-
-        {
-          id: 5,
-          title: "Wimmelbilder",
-          image: "../assets/wimmel.png",
-          videoSrc:
-            "https://www.youtube.com/embed/X6CBm3JiTpw?si=oEIt9r47LMV1-K0H",
-          description:
-            "Very simple where's waldo inspired game, has fun features like leaderboard",
-          appHref: "https://main--nimble-cendol-ef155b.netlify.app/#/",
-          githubHref: "https://github.com/JasonCqq/Wimmelbilder",
-          iconsHTML: ["React", "Typescript", "Firebase"],
-          showVid: false,
-        }
-      );
-    } else if (this.show === false) {
-      this.projects.pop();
-      this.projects.pop();
-      this.projects.pop();
-    }
-  }
-
   projects: Project[] = [
     {
       id: 0,
@@ -149,5 +95,46 @@ export class AppComponent {
       iconsHTML: ["Figma"],
       showVid: false,
     },
+
+    {
+      id: 3,
+      title: "minBlog",
+      image: "../assets/23.png",
+      videoSrc: "https://www.youtube.com/embed/J5s9PAta4V0?si=TyCnC-xOPs1JOwRn",
+      description:
+        "Blog sharing and reading platform with short blogs (max 1500 chars). With features such as user authentication, and basic CMS features: user profiles, blog searching, commenting, filtering, and pagination",
+      appHref: "https://minblog21715.netlify.app/",
+      githubHref: "https://github.com/JasonCqq/minBlog",
+      iconsHTML: ["React", "MongoDB", "Express", "Node", "Typescript"],
+      showVid: false,
+    },
+
+    {
+      id: 4,
+      title: "Jwitter",
+      image: "../assets/jwitter.png",
+      videoSrc:
+        "https://www.youtube.com/embed/hJ5OKDj8ZxE?si=3VvpDTXYR4fWYPD_",
+      description:
+        "Twitter clone that replicates the core features of old Twitter. This project was developed from scratch without relying on tutorials to practice full stack skills.",
+      appHref: "https://main--chipper-gnome-4de1e9.netlify.app/",
+      githubHref: "https://github.com/JasonCqq/Jwitter",
+      iconsHTML: ["React", "Typescript", "Firebase"],
+      showVid: false,
+    },
+
+    {
+      id: 5,
+      title: "Wimmelbilder",
+      image: "../assets/wimmel.png",
+      videoSrc:
+        "https://www.youtube.com/embed/X6CBm3JiTpw?si=oEIt9r47LMV1-K0H",
+      description:
+        "Very simple where's waldo inspired game, has fun features like leaderboard",
+      appHref: "https://main--nimble-cendol-ef155b.netlify.app/#/",
+      githubHref: "https://github.com/JasonCqq/Wimmelbilder",
+      iconsHTML: ["React", "Typescript", "Firebase"],
+      showVid: false,
+    }
   ];
 }
